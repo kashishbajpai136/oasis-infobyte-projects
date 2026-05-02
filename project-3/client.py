457615937bb500b8e335ac8ba258207b2612219e
kashishbajpai136 <kashishbajpai325@gmail.com>
2026-05-02 19:46:30 +0530

    Add files via upload

---
+++ project-3/client.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = input("")
+        client.send(message.encode('utf-8'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
